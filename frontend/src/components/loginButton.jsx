0b3be7657d5ca1f90bf67667b555a4f51bfe313e
gootieno <geoff.otieno7@gmail.com>
2020-07-07 10:05:19 -0700

    styling the navbar

--- frontend/src/components/loginButton.jsx
+++ frontend/src/components/loginButton.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import "./loginButton.css"
 
 const LoginButton = () => {
   const { loginWithRedirect } = useAuth0();
@@ -9,30 +10,6 @@ const LoginButton = () => {
       Log In
     </button>
   );
-  //   <button className="login-button" onClick={() => loginWithRedirect()}>
-  //     Log In
-  //   </button>
-  // );
-  //   <button className="login-button" onClick={() => loginWithRedirect()}>
-  //     Log In
-  //   </button>
-  // );
-  //   <button className="login-button" onClick={() => loginWithRedirect()}>
-  //     Log In
-  //   </button>
-  // );
-  //   <button className="login-button" onClick={() => loginWithRedirect()}>
-  //     Log In
-  //   </button>
-  // );
-  //   <button className="login-button" onClick={() => loginWithRedirect()}>
-  //     Log In
-  //   </button>
-  // );
-  //   <button className="login-button" onClick={() => loginWithRedirect()}>
-  //     Log In
-  //   </button>
-  // );
 };
 
 export default LoginButton;
