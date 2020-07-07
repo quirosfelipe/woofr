be47d97c0c996296827b6ef606329bd96b51b952
gootieno <geoff.otieno7@gmail.com>
2020-07-07 13:08:26 -0700

    made the nav bar and cleaning up the formating

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -3,6 +3,8 @@ import { Link } from "react-router-dom";
 import Login from "./login";
 import "./navbar.css";
 import Search from "./search";
+
+
 const Navbar = () => (
   <div className="landing-page-navbar positioned">
     <div className="landing-page-navbar__header">
@@ -10,10 +12,9 @@ const Navbar = () => (
         <h1>Woofr</h1>
       </Link>
     </div>
-    <Search />
-    {/* <div className="landing-page-navbar__header">
-      <input alt="Search"></input>
-    </div> */}
+    <div className="landing-page-navbar__header">
+      <Search />
+    </div>
     <Link className="landing-page-navbar__header">
       <Login />
     </Link>
--- frontend/src/components/search.jsx
+++ frontend/src/components/search.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import './search.css'
 
 const Search = () => {
   const [search, setSearch] = useState("");
