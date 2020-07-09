e64ec7ad2547c3452427dfb534c06429153d1d00
jank_ <jank_329@hotmail.com>
2020-07-08 23:52:48 -0500

    Photo page

---
+++ frontend/src/components/username-box.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import './username-box.css'
+
+
+const UserNameBox = () => {
+
+  return (
+    <div className="username-container">
+        <div className="picture">
+            <img src="http://farm66.staticflickr.com/65535/50084651468_07fa3ef705.jpg"></img>
+        </div>
+        <div className="info">
+            <div>Username</div>
+            <div>nickname</div>
+            <div>Bio</div>
+        </div>
+        <div className="follow">
+            <button>FOLLOW</button>
+        </div>
+        <div className="blank"></div>
+        <div className="widgets">
+            <div>
+                <button>Like</button>
+            </div>
+            <div>
+                <button>Github</button>
+            </div>
+
+        </div>
+
+    </div>
+  );
+};
+export default UserNameBox;
+//"picture" "info" "follow" "blank" "widgets";
