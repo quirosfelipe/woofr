bb4a7e9bf40f042808f65109f373b4fadf1154f9
gootieno <geoff.otieno7@gmail.com>
2020-07-10 10:17:58 -0700

    created aws files for python

---
+++ backend/aws/aws_s3.py
@@ -0,0 +1,37 @@
+import boto3
+
+#using aws s3
+s3 = boto3.resource('s3')
+
+def upload_file(file_name, bucket):
+    """
+    Function to upload a file to an S3 bucket
+    """
+    object_name = file_name
+    s3_client = boto3.client('s3')
+    response = s3_client.upload_file(file_name, bucket, object_name)
+
+    return response
+
+
+def download_file(file_name, bucket):
+    """
+    Function to download a given file from an S3 bucket
+    """
+    s3 = boto3.resource('s3')
+    output = f"downloads/{file_name}"
+    s3.Bucket(bucket).download_file(file_name, output)
+
+    return output
+    
+
+def list_files(bucket):
+    """
+    Function to list files in a given S3 bucket
+    """
+    s3 = boto3.client('s3')
+    contents = []
+    for item in s3.list_objects(Bucket=bucket)['Contents']:
+        contents.append(item)
+
+    return contents
