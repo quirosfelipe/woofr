00645155fd1fbc93b8b34053e238b69a20e5e8d1
Felipe Quiros <quirosfelipe@gmail.com>
2020-08-02 18:45:28 -0400

    fixing deployment

--- frontend/src/config.js
+++ frontend/src/config.js
@@ -1,2 +1,4 @@
 export const baseUrl =
-  process.env.NODE_ENV === "development" ? "http://localhost:5000/api" : "/api";
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:5000/api"
+    : "https://woofrapp.herokuapp.com/api";
