67241a1a777323a77a855a17db403758f66fce1e
gootieno <geoff.otieno7@gmail.com>
2020-08-10 23:45:02 -0700

    cleaned up the form data and successfully wired up the aws route. Minor issue with aws account not allowing us to post. Code works though

--- frontend/src/store/utils/apiUtil.js
+++ frontend/src/store/utils/apiUtil.js
@@ -1,51 +1,44 @@
-import { puppiesAdded } from "../reducers/puppyPhotos";
-import { puppyAdded } from "../reducers/puppyPhotos";
-import { photoUploaded } from "../reducers/aws";
-import { myPuppy } from "../reducers/puppyPhotos";
+import { puppiesAdded } from '../reducers/puppyPhotos';
+import { puppyAdded } from '../reducers/puppyPhotos';
+import { photoUploaded } from '../reducers/aws';
+import { myPuppy } from '../reducers/puppyPhotos';
 
 export const fetchPuppyPhotos = () => async (dispatch) => {
-  const response = await fetch(`http://localhost:5000/api/photos`);
-  if (!response.ok) throw response;
-  const { photos } = await response.json();
-  dispatch(puppiesAdded(photos));
+	const response = await fetch(`http://localhost:5000/api/photos`);
+	if (!response.ok) throw response;
+	const { photos } = await response.json();
+	dispatch(puppiesAdded(photos));
 };
 
 export const fetchPuppyPhoto = (id) => async (dispatch) => {
-  const response = await fetch(`http://localhost:5000/api/photos/photo/${id}`);
-  if (!response.ok) throw response;
-  console.log("This is the response", response);
-  const { photo, comments } = await response.json();
-  photo.comments = comments;
-  let payload = { ...photo };
-  dispatch(puppyAdded(payload));
+	const response = await fetch(`http://localhost:5000/api/photos/photo/${id}`);
+	if (!response.ok) throw response;
+	console.log('This is the response', response);
+	const { photo, comments } = await response.json();
+	photo.comments = comments;
+	let payload = { ...photo };
+	dispatch(puppyAdded(payload));
 };
 
 export const postToAws = (formData, userId, description, photoName) => async (
-  dispatch
+	dispatch
 ) => {
-  console.log("in the aws route", description, photoName);
-  let response = await fetch(`http://localhost:5000/api/${userId}/upload`, {
-    method: "POST",
-    body: formData,
-  });
-  if (response.ok) {
-    const { photoUrl } = await response.json();
-    console.log("photourl -> ", photoUrl);
-    const message = "upload success";
-    await fetch(`http://localhost:5000/api/photos/create`, {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify({ userId, description, photoName, photoUrl }),
-    });
-    window.location.href = "/profile-page";
-    return dispatch(photoUploaded(message));
-  }
-  console.log("res not okay");
+	console.log('in the aws route', description, photoName);
+	let response = await fetch(`http://localhost:5000/api/${userId}/upload`, {
+		method: 'POST',
+		body: formData,
+	});
+	if (response.ok) {
+		const message = 'upload success';
+		window.location.href = '/profile-page';
+		return dispatch(photoUploaded(message));
+	}
+	
 };
 
 export const fetchPuppyOwners = (id) => async (dispatch) => {
-  const response = await fetch(`http://localhost:5000/api/photos/${id}`);
-  if (!response.ok) throw response;
-  const { photos } = await response.json();
-  dispatch(myPuppy(photos));
+	const response = await fetch(`http://localhost:5000/api/photos/${id}`);
+	if (!response.ok) throw response;
+	const { photos } = await response.json();
+	dispatch(myPuppy(photos));
 };
