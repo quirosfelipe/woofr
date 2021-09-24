2010abd9de10fa5010867843cecc6c2e7779e912
geoffrey otieno <gotieno@geoffreys-MBP.attlocal.net>
2021-09-24 12:19:12 -0700

    refactored redux configuration in store/index

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,19 +1,29 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { Provider } from 'react-redux';
-import configureStore from './store/configureStore';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import configureStore from "./store/index";
 
 const store = configureStore();
 
-document.addEventListener('DOMContentLoaded', () => {
-	ReactDOM.render(
-		<React.StrictMode>
-			<Provider store={store}>
-				<App />
-			</Provider>
-		</React.StrictMode>,
-		document.getElementById('root')
-	);
-});
+if (process.env.NODE_ENV !== "production") {
+  window.store = store;
+}
+
+function Root() {
+  return (
+    <Provider store={store}>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </Provider>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <Root />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
--- frontend/src/store/index.js
+++ frontend/src/store/index.js
@@ -4,19 +4,23 @@ import { authReducer } from "./reducers/auth";
 import { puppyPhotoReducer } from "./reducers/puppyPhotos";
 import { photoUploadedReducer } from "./reducers/aws";
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-
 const reducers = combineReducers({
   session: authReducer,
   photos: puppyPhotoReducer,
   uploaded: photoUploadedReducer,
 });
 
-const configureStore = (initialState = {}) => {
-  return createStore(
-    reducers,
-    initialState,
-    composeEnhancers(applyMiddleware(thunk))
-  );
+let enhancer;
+
+if (process.env.NODE_ENV !== "production") {
+  const logger = require("redux-logger").default;
+  const composeEnhancers =
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+  enhancer = composeEnhancers(applyMiddleware(logger, thunk));
+}
+
+const configureStore = (preloadedState) => {
+  return createStore(reducers, preloadedState, enhancer);
 };
+
 export default configureStore;
