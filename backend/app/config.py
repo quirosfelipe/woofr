352c086ff2fa72a4fd329fcc0cf7a1196f8071ba
gootieno <geoff.otieno7@gmail.com>
2021-09-27 15:15:26 -0700

    created local database and aws environment variables pointing to a new s3 bucket

--- backend/app/config.py
+++ backend/app/config.py
@@ -9,7 +9,7 @@ S3_LOCATION = 'http://{}.s3-us-west-1.amazonaws.com/'.format(S3_BUCKET)
 
 class Configuration:
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        "DATABASE_URL") or "postgresql://woofr:woofr@localhost/woofr_db"
+        "DATABASE_URL") or "postgresql://woofr:password@localhost/woofr_db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = "klisafl4735628765uiasd654363456fiuh"
     S3_BUCKET = os.environ.get("S3_BUCKET_NAME")
